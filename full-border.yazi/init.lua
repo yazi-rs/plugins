eaa682df64040813ff72e3f2f2b2f0c7306d0929
ocl2go <106097501+ocl2go@users.noreply.github.com>
2024-06-29 21:48:12 +0900

    feat: allow customization of full-border color via theme setting

--- full-border.yazi/init.lua
+++ full-border.yazi/init.lua
@@ -1,7 +1,6 @@
 local function setup()
 	Manager.render = function(self, area)
 		local c = self:layout(area)
-
 		local bar = function(c, x, y)
 			if x <= 0 or x == area.w - 1 then
 				return {}
@@ -13,11 +12,12 @@ local function setup()
 			):symbol(c)
 		end
 
+		local style = THEME.manager.border_style
 		return ya.flat {
 			-- Borders
-			ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-			ui.Bar(c[1]:padding(ui.Padding.y(1)), ui.Bar.RIGHT),
-			ui.Bar(c[3]:padding(ui.Padding.y(1)), ui.Bar.LEFT),
+			ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED):style(style),
+			ui.Bar(c[1]:padding(ui.Padding.y(1)), ui.Bar.RIGHT):style(style),
+			ui.Bar(c[3]:padding(ui.Padding.y(1)), ui.Bar.LEFT):style(style),
 
 			bar("┬", c[1].right - 1, c[1].y),
 			bar("┴", c[1].right - 1, c[1].bottom - 1),
