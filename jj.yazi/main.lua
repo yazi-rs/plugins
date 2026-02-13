e81e9803ce970f5d7beaf2e107e0f502d53dff9a
LilDojd <yawner@pm.me>
2026-02-13 14:10:25 +0400

    chore: ya.render() -> ui.render()
    Signed-off-by: LilDojd <yawner@pm.me>
    Change-Id: I3beccb48fb8072006a53f908c7ae23486a6a6964

--- jj.yazi/main.lua
+++ jj.yazi/main.lua
@@ -177,7 +177,7 @@ local add = ya.sync(function(st, cwd, repo, changed)
 			st.repos[repo][path] = code
 		end
 	end
-	ya.render()
+	ui.render()
 end)
 
 ---@param cwd string
@@ -188,7 +188,7 @@ local remove = ya.sync(function(st, cwd)
 		return
 	end
 
-	ya.render()
+	ui.render()
 	st.dirs[cwd] = nil
 	if not st.repos[repo] then
 		return
