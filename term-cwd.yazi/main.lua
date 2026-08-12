701502fa0a5c9b62e18ad4bff9973e11d351cba8
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2026-08-12 19:38:52 +0200

    uri encode on term-cwd OSC7

--- term-cwd.yazi/main.lua
+++ term-cwd.yazi/main.lua
@@ -8,6 +8,13 @@ local function setup(_, opts)
     local writers = {
         -- Recommended for unix
         OSC7 = function(cwd)
+            -- Convert and percent-encode the path to a valid file URI for OSC 7.
+            if target == "windows" then
+                cwd = "/" .. cwd:gsub("\\", "/")
+            end
+            cwd = cwd:gsub("([^%w%-%._~/])", function(char)
+                return string.format("%%%02X", char:byte())
+            end)
             io.write("\x1b]7;file://localhost" .. cwd .. "\x1b\\")
         end,
         -- Recommended for windows
