efa4d79da8ada35380ede5788d3f3b0ee9f70306
Hao Ren Zhou <104418647+haoren-zhou@users.noreply.github.com>
2026-08-20 02:09:12 +0800

    feat(git)!: separate staged and unstaged modified states (#218)
    `modified` and `modified_sign` have been renamed to `untracked` and `untracked_sign` respectively to align the terms used with Git.
    
    ---------
    
    Co-authored-by: 三咲雅 misaki masa <sxyazi@gmail.com>

--- git.yazi/main.lua
+++ git.yazi/main.lua
@@ -9,9 +9,10 @@ local WINDOWS = ya.target_family() == "windows"
 local CODES = {
 	unknown = 100, -- status cannot/not yet determined
 	excluded = 99, -- ignored directory
-	ignored = 6, -- ignored file
-	untracked = 5,
-	modified = 4,
+	ignored = 7, -- ignored file
+	untracked = 6,
+	unstaged = 5,
+	staged = 4,
 	added = 3,
 	deleted = 2,
 	updated = 1,
@@ -21,7 +22,8 @@ local CODES = {
 local PATTERNS = {
 	{ "!$", CODES.ignored },
 	{ "?$", CODES.untracked },
-	{ "[MT]", CODES.modified },
+	{ ".[MT]", CODES.unstaged },
+	{ "[MT] ", CODES.staged },
 	{ "[AC]", CODES.added },
 	{ "D", CODES.deleted },
 	{ "U", CODES.updated },
@@ -34,7 +36,8 @@ local function theme()
 		[CODES.unknown] = t.unknown or ui.Style(),
 		[CODES.ignored] = t.ignored or ui.Style():fg("darkgray"),
 		[CODES.untracked] = t.untracked or ui.Style():fg("magenta"),
-		[CODES.modified] = t.modified or ui.Style():fg("yellow"),
+		[CODES.unstaged] = t.unstaged or ui.Style():fg("yellow"),
+		[CODES.staged] = t.staged or ui.Style():fg("green"),
 		[CODES.added] = t.added or ui.Style():fg("green"),
 		[CODES.deleted] = t.deleted or ui.Style():fg("red"),
 		[CODES.updated] = t.updated or ui.Style():fg("yellow"),
@@ -43,7 +46,8 @@ local function theme()
 		[CODES.unknown] = t.unknown_sign or "",
 		[CODES.ignored] = t.ignored_sign or " ",
 		[CODES.untracked] = t.untracked_sign or "? ",
-		[CODES.modified] = t.modified_sign or " ",
+		[CODES.unstaged] = t.unstaged_sign or " ",
+		[CODES.staged] = t.staged_sign or " ",
 		[CODES.added] = t.added_sign or " ",
 		[CODES.deleted] = t.deleted_sign or " ",
 		[CODES.updated] = t.updated_sign or " ",
