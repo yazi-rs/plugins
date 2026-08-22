72f9e3c007956c122d8657f6d39c78e7585a4718
Tyarel8 <98483313+Tyarel8@users.noreply.github.com>
2026-08-22 14:36:59 +0200

    fix(term-cwd): missing slash for OSC7 on windows (#229)

--- term-cwd.yazi/main.lua
+++ term-cwd.yazi/main.lua
@@ -8,6 +8,10 @@ local function setup(_, opts)
     local writers = {
         -- Recommended for unix
         OSC7 = function(cwd)
+            -- Add starting slash on windows to separate the drive letter from hostname
+            if target == "windows" then
+                cwd = "/" .. cwd
+            end
             io.write("\x1b]7;file://localhost" .. cwd .. "\x1b\\")
         end,
         -- Recommended for windows
